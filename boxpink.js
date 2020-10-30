83c38ed59fd3abacc7ed04f1de3441baa7a900cf
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-10-30 23:26:38 +0530

    child class 3

---
+++ boxpink.js
@@ -0,0 +1,14 @@
+class Box1 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill(230, 190, 234)
+    super.display();
+  }
+}
+     
