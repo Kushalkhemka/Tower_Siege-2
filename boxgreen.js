cd1c245f90d79b24c22728c967c7d57442fef157
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-11-01 00:23:36 +0530

    vanish effect

--- boxgreen.js
+++ boxgreen.js
@@ -3,11 +3,28 @@ class Box2 extends BaseClass
   constructor(x,y,width,height)
   {
     super(x,y,width,height);
+    this.image=loadImage("block4.png")
+    this.Visibility=255;
   }
 
   display()
   {
-    fill(60, 242, 163);
-    super.display();
+    if(this.body.speed>2.5)
+    {
+      World.remove(world,this.body);
+      push();
+      this.Visibility=this.Visibility-5;
+      tint(255,this.Visibility);
+      var pos=this.body.position;
+      image(this.image,pos.x,pos.y,this.width,this.height)
+      pop();
+    }
+    else
+    {
+      imageMode(CENTER)
+      image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+      super.display();
+    }
   }
+
 }
